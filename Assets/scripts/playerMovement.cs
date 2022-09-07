f2f85fc44b69b9bac7b6ba45b13231862c53ee13
Stokbrootje <73719051+Stokbrootje@users.noreply.github.com>
2022-09-07 19:10:14 +0200

    dodge en richten

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -5,21 +5,70 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float activeMoveSpeed;
+    public float dashSpeed;
+    public float dashlenght = 0.5f, dashcooldown = 1f;
+
+    public float dashCounter;
+    public float dashcoolCounter;
+
     Vector2 movement;
+    Vector2 mousePos;
 
-    [SerializeField] private Rigidbody2D rb;
+    public Rigidbody2D rb;
+    public Camera cam;
+
+
+    void Start()
+    {
+        activeMoveSpeed = moveSpeed;
+    }
     
-
     // Update is called once per frame
     void Update()
     {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashcoolCounter <=0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashlenght;
+            }
+        }
+
+       if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashcoolCounter = dashcooldown;
+            }
+
+            if (dashcoolCounter > 0)
+            {
+                dashcoolCounter -= Time.deltaTime;
+            }
+        }
+       
        movement.x = Input.GetAxisRaw("Horizontal");
-       movement.x = Input.GetAxisRaw("Vertical");
+       movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+       
+
+     
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        rb.MovePosition(rb.position + movement * activeMoveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 
 }
