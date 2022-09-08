f795bfec2701fb8e7affd30d131bd33d2d5e2373
EdRma <34298@ma-web.nl>
2022-09-08 13:21:01 +0200

    WeaponPickUp

--- Assets/scripts/WeaponPickUp.cs
+++ Assets/scripts/WeaponPickUp.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class WeaponPickUp : MonoBehaviour
 {
- public
+    public WeaponPickUp weapon;
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Player")
+        {
+            target.GetComponent<Player>().currentweapon = weapon;
+            target.transform.GetChild(3).GetComponent<SpriteRenderer>.sprite = weapon.currentWeaponSpr;
+            Destroy(gameObject);
+        }
+    }
 }
