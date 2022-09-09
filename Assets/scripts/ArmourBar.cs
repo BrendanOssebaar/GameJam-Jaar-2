2fa502eafb87e2639c210d4fa27a437cc8bee3ff
AsBas <90893787+sappieman@users.noreply.github.com>
2022-09-09 14:28:23 +0200

    ArmourBar

---
+++ Assets/scripts/ArmourBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ArmourBar : MonoBehaviour
+{
+    private GameObject Image;
+    private bool Armour;
+
+    public void SwitchArmourBar()
+    {
+        if (Armour = true)
+        {
+            Image.SetActive(true);
+        }
+
+
+        else (Armour = false);
+        {
+            Image.SetActive(false);
+        }
+    }
+    
+
+    
+}
