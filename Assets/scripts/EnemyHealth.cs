98aeccb819f5912172036bbe6d5cd1192924f8b2
spookyboy2000 <54852313+spookyboy2000@users.noreply.github.com>
2022-09-14 09:13:08 +0200

    Random item

--- Assets/scripts/EnemyHealth.cs
+++ Assets/scripts/EnemyHealth.cs
@@ -26,8 +26,16 @@ public class EnemyHealth : MonoBehaviour
 
 	void Die()
 	{
-		//Instantiate(deathEffect, transform.position, Quaternion.identity);
+		if(Random.Range(0f, 1f) <= m_dropChance)
+        {
+			// spawn item
+			Instantiate(deathEffect, transform.position, Quaternion.identity);
+		}
+
+
 		Destroy(gameObject);
 	}
+
+	const float m_dropChance = 1f / 10f; // Set Odds here - e.g. 1 in 10 chance
 
 }
